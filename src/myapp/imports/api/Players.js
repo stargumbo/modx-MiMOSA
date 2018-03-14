63e2888da857fcd7f5a802662848ea980a3b5fa0
root <root@util222.mia2.cbsig.net>
2018-03-14 18:31:54 +0000

    Backup util222 3/14/2018 - 14:31

--- src/myapp/imports/api/Players.js
+++
@@ -1,1 +0,0 @@
-export const Players = new Mongo.Collection('players');
--- src/myapp/server/main.js
+++
@@ -1,6 +0,0 @@
-import { Meteor } from 'meteor/meteor';
-import { Players } from '/imports/api/Players';
-
-Meteor.startup(() => {
-  // code to run on server at startup
-});
